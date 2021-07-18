f3bacbb607ff19b9cb338ce9a5fd3d1e3d5555fe
Svitlana Trush <svittrush18@gmail.com>
2021-07-18 23:32:36 +0300

    Adds logic in APP

---
+++ src/Searchbar/Searchbar.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+export class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      query: e.target.value,
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ query: '' });
+  };
+  render() {
+    return (
+      <header className="Searchbar">
+        <form className="SearchForm" onSubmit={this.handleSubmit}>
+          <button type="submit" className="SearchForm-button">
+            <span className="SearchForm-button-label">Search</span>
+          </button>
+
+          <input
+            className="SearchForm-input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleInputChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
